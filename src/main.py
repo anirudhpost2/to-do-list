789296681d5080f7331f0fe8a421b5ba068054a0
Anirudh Venkataraghavan <anirudh.post@gmail.com>
2026-05-21 16:12:59 -0400

    Implmeneted main...

---
+++ src/main.py
@@ -0,0 +1,21 @@
+from .tasks.Task import Task
+from .tasks.TimedTask import TimedTask
+from .list.ToDoList import ToDoList
+from datetime import datetime
+
+def main():
+    my_list = ToDoList()
+    my_list.add_task(Task("Study for Data Structures"))
+    my_list.add_task(TimedTask("Upload singing content", "2026-05-22"))
+    my_list.add_task(TimedTask("Figure out what to do with my life", "2026-05-20"))
+
+    my_list.display_tasks()
+
+    my_list.tasks[0].mark_completed()
+    my_list.tasks[1].mark_completed()
+    my_list.tasks[2].mark_completed()
+
+    my_list.display_tasks()
+
+if __name__ == "__main__":
+    main()
